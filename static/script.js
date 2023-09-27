e40b5aa7faa02274cdb7da3233e9b7992426b3a2
Tianrider <hadiwijayachristian7@gmail.com>
2023-09-27 20:55:16 +0700

    first commit

---
+++ static/script.js
@@ -0,0 +1,13 @@
+
+function scroll_to_top() {
+    const smoothscroll = () => {
+        const currentScroll = 
+        document.documentElement.scrollTop || document.body.scrollTop;
+
+        if (currentScroll > 0) {
+            window.requestAnimationFrame(smoothscroll);
+            window.scrollTo(0, currentScroll - (currentScroll / 8));
+        }
+    };
+    smoothscroll();  
+}
